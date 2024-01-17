0d302fdc134e640cd33726f14392f0c04d5835f6
Mohammed-Jarrad <mohammed.w.jarrad@gmail.com>
2024-01-17 02:13:14 +0200

    change calidation letters length

--- src/modules/category/category.validation.js
+++ src/modules/category/category.validation.js
@@ -2,15 +2,15 @@ import joi from 'joi'
 import { generalFields } from '../../middlewares/validation.js'
 
 export const createCategory = joi.object({
-	name_en: joi.string().min(3).max(25).label('English name'),
-	name_ar: joi.string().min(3).max(25).required().label('Arabic name'),
+	name_en: joi.string().min(3).max(50).label('English name'),
+	name_ar: joi.string().min(3).max(50).required().label('Arabic name'),
 	description_ar: joi.string().max(300).label('Arabic description'),
 	description_en: joi.string().max(300).label('English description'),
 	file: generalFields.file.required().label('image'),
 })
 export const updateCategory = joi.object({
-	name_en: joi.string().min(3).max(25).label('English name'),
-	name_ar: joi.string().min(3).max(25).label('Arabic name'),
+	name_en: joi.string().min(3).max(50).label('English name'),
+	name_ar: joi.string().min(3).max(50).label('Arabic name'),
 	description_ar: joi.string().max(300).label('Arabic description'),
 	description_en: joi.string().max(300).label('English description'),
 	file: generalFields.file.label('image'),
